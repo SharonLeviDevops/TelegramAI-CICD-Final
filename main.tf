32640824be412a709fa3b07f99c3e7a74ab3d566
sharonlevi <sharont55l@gmail.com>
2023-04-16 11:29:05 +0300

    update main tf

--- main.tf
+++ main.tf
@@ -1,14 +1,16 @@
 ######################################
 # Terraform Aws Cloud infrastructure #
 ######################################
-
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
 provider "aws" {
   region     = "us-east-2"
 }
-
-module "docker" {
-  source  = "registry.terraform.io/hashicorp/docker"
-  version = "2.15.0"
 }
 
 # Copy AMIs from another region
