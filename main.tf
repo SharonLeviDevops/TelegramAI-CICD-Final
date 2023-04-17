73385275ffcb8bbc0c8bf6942601d196194436ad
Levi Home <sharont55l@gmail.com>
2023-04-17 15:11:01 +0300

    changes in dirs

--- main.tf
+++ main.tf
@@ -178,6 +178,12 @@ resource "aws_security_group" "terraform-securitygp-exr" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    egress {
+          from_port = 0
+          to_port = 0
+          protocol = "all"
+          cidr_blocks = ["0.0.0.0/0"]
+    }
       tags = {
         Name = "terraform-securitygp-exr"
       }
